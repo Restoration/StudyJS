8db20c972d2b3c88b1b126f690633d7e7d7ac0d2
RyotArch <bboy.ryu@icloud.com>
2018-10-19 19:31:22 -0700

    [Edit] Create modal window
    Implement modal window

---
+++ modal-window-plugin/ModalWindow.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById('modalWindow');
+var open = document.getElementById('open');
+var close = document.getElementById('close');
+
+function show(){
+    var overLay = document.createElement('div');
+    overLay.setAttribute("id","overLay");
+    document.body.append(overLay);
+    overLay.addEventListener('click',hide);
+    modal.style.display = "block";
+}
+
+function hide(){
+    overLay.remove();
+    modal.style.display = "none";
+}
+
+function handler(){
+    open.addEventListener('click',show);
+    close.addEventListener('click',hide);
+}
+
+function init(){
+    handler();
+}
+init();
