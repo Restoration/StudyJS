83ebc83835b48d5dc3b421679a650e5014c2f80e
RyotArch <bboy.ryu@icloud.com>
2018-10-22 04:41:27 -0700

    [Add] Center function
    Create source

--- modal-window-plugin/ModalWindow.js
+++ modal-window-plugin/ModalWindow.js
@@ -8,11 +8,23 @@ function show(){
     document.body.append(overLay);
     overLay.addEventListener('click',hide);
     modal.style.display = "block";
+    center(modal);
 }
 
 function hide(){
     overLay.remove();
     modal.style.display = "none";
+}
+
+function center(elem){
+    var height = elem.clientHeight;
+    var width = elem.clientWidth;
+    elem.style.top = '50%';
+    elem.style.left = '50%';
+    console.log(height);
+    console.log(width);
+    elem.style.marginTop = '-'+(height/2)+'px';
+    elem.style.marginLeft = '-'+(width/2)+'px';
 }
 
 open.addEventListener('click',show);
